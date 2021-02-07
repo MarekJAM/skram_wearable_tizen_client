e0738f84199b2d1550f0ffc7a1c28487632821d0
MarekJAM <marekash@gmail.com>
2021-02-07 22:55:16 +0100

    Login screen sketch done

--- js/app.js
+++ js/app.js
@@ -20,3 +20,30 @@
 		}
 	});
 }());
+
+function showLoader() {
+	  document.getElementById("overlay").style.display = "block";
+	}
+
+function hideLoader() {
+	  document.getElementById("overlay").style.display = "none";
+	}
+
+function call() {
+	const data = JSON.stringify({
+		  username: "Eeee"
+		});
+			
+		var xhr = new XMLHttpRequest();
+		
+		xhr.addEventListener('readystatechange', function() {
+		  if (this.readyState === this.DONE) {
+		    alert(this.responseText);
+		  }
+		});
+
+		xhr.open('POST', 'http://192.168.0.25:8080/auth/login/guest', true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		
+		xhr.send(data);
+}
